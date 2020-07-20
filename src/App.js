ca642882c5d803d095cb4bd76b42db51c51d25fc
EC2 Default User <ec2-user@ip-172-31-45-207.ap-south-1.compute.internal>
2020-07-20 13:39:04 +0000

    app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi kasi, I was changed on the feature<code>src/App.js</code> and save to reload.
+          Hi Balaji<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
